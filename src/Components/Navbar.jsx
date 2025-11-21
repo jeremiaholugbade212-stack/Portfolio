7fbcd2f8d7d96dd3195579e59ffea87d1745067f
jeremiah olugbade <kunleolugbade146@gmail.com>
2025-11-21 19:14:16 +0100

    updated

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
       <div className="py-3 flex justify-between items-center">
 
         {/* Logo */}
-        <div className="text-2xl font-bold">JEREMIAH OLUGBADE</div>
+        <div className="text-xl font-bold">JEREMIAH OLUGBADE</div>
 
         {/* Mobile Menu Icon */}
         <button 
