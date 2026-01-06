da5287b2e666f73f7aa611beb77d24dbbd3d25ef
jeremiah olugbade <kunleolugbade146@gmail.com>
2026-01-06 11:51:45 +0100

    push

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -31,6 +31,12 @@ const projects = [
     link: "https://medical-wupl.vercel.app/",
     image: medicalImg,
   },
+  {
+    title: "Medical Booking App",
+    description: "A responsive healthcare platform built with React and Tailwind CSS.",
+    link: "https://medical-wupl.vercel.app/",
+    image: medicalImg,
+  },
 ];
 
 const Projects = () => {
