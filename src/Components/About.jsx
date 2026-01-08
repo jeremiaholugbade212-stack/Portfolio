5fa04cdb8398f01d73d9698bcaecb37e7a7d4d85
jeremiah olugbade <kunleolugbade146@gmail.com>
2026-01-08 12:20:16 +0100

    updated

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -8,7 +8,7 @@ const About = () => {
 
         {/* Heading */}
         <h2 className='text-4xl font-bold text-center mb-12'>
-          About Mee
+          About Me
         </h2>
 
         {/* Content */}
